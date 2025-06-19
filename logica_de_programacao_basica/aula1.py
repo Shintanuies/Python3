d725799bc1e1e002a2c2113c168cd87630673957
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-19 10:37:13 -0300

    edit aula2.py

--- logica_de_programacao_basica/aula1.py
+++ logica_de_programacao_basica/aula1.py
@@ -1,4 +1,15 @@
-# Permite escrever um comentário
+"""
+DocString = Não é exatamente um comentário
+Permite escrever comentarios multi linhas - Este tipo de comentário é lido pelo interpretador
+
+"""
+
+'''
+Também pode ser usado com aspas simples
+
+'''
+
+# Permite escrever um comentário - Este tipo de comentário o interpretador do python nçao lê
 print(123)  # Ao lado
 # Abaixo
 print(123)
---
+++ logica_de_programacao_basica/aula2.py
@@ -0,0 +1,13 @@
+# Função print
+
+print(12, 34)
+print(56, 78)
+
+# Mudando o separador
+
+print(12, 34, sep='-', end='##\n')  # Pode usar-se aspas simples ou duplas
+print(56, 78, sep="-", end='\n')
+print(9, 10, sep="-", end='\n')
+
+# \r \n -> CRLF
+# \n -> LF
