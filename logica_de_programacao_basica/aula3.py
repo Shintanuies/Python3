daa2d2c3488727ccd331fdc9d81b8ac6d8b5b181
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-19 11:06:36 -0300

    edit aula3.py

---
+++ logica_de_programacao_basica/aula3.py
@@ -0,0 +1,23 @@
+"""
+Python -> Linguagem de Programação
+Tipo de Tipagem -> Dinâmica \ Forte
+
+str -> string -> texto
+Strings sâo textos que estâo dentro de aspas
+
+"""
+print(1234)
+
+# Aspas Simples
+print('Bruno Vidal')
+print(1, 'Bruno "Vidal"')  # Aspas dentro de Aspas
+
+# Aspas Duplas
+print("Bruno Vidal")
+print(2, "Bruno 'Vidal'")
+
+# Escape
+print('Bruno \"Vidal\"')
+
+# r
+print(r'Bruno \"Vidal\"')
