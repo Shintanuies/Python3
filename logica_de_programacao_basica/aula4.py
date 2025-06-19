bd9cc9ee69b32b3245fb501990c7557b0fb8bccd
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-19 20:23:29 -0300

    include aula4.py

---
+++ logica_de_programacao_basica/aula4.py
@@ -0,0 +1,38 @@
+# TIPOS DE DADOS:
+
+# Tipos int e float
+# int -> Número inteiro
+# O tipo inteiro representa qualquer número
+# positivo ou negativo. int sem sinal é considerado
+# positivo.
+print('int:')
+print('')
+
+print(11)  # int
+print(-11)  # int
+print(0)  # int
+
+print('___________________')
+
+# float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+print('float:')
+print('')
+
+print(1.1, 10.11)
+
+print('___________________')
+
+# A função type mostra o tipo que o python
+# inferiu ao valor.
+print('função type(classe type):')
+print('')
+
+print(type('Bruno'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0))
+print(type('Bruno'))
+
+print('___________________')
