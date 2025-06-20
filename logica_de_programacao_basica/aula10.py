4047ba31282db171b98799167656e845c784a030
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-20 00:53:24 -0300

    include aula10.py

---
+++ logica_de_programacao_basica/aula10.py
@@ -0,0 +1,11 @@
+#  Concatenação (+) e repetição (*) com operadores
+#  aritméticos
+
+concatenacao = 'Bruno' + ' ' + 'Vidal'
+print(concatenacao)
+
+a_dez_vezes = 'A ' * 10
+print(a_dez_vezes)
+
+tres_vezes_bruno = 3 * 'Bruno '
+print(tres_vezes_bruno)
