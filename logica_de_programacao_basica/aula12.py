7c84c9bb6ee8a47df40d249d21e780b93f61052b
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-20 09:10:05 -0300

    include aula12.py

---
+++ logica_de_programacao_basica/aula12.py
@@ -0,0 +1,15 @@
+# Exercício:
+# Cálculo do IMC
+
+nome = 'Bruno Gomes'
+altura = 1.72
+peso = 82
+imc = peso / (altura * altura)
+print('Nome:', nome)
+print('IMC:', imc)
+print('-------------------------')
+print(nome, 'tem', altura, 'de altura, pesa', peso, 'quilos e seu IMC é', imc)
+print('_________________________')
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é')
+print(imc)
