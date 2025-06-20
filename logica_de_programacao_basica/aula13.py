3544c0c1e90948917542b602634de4fa9b878361
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-20 09:27:59 -0300

    include aula13.py

---
+++ logica_de_programacao_basica/aula13.py
@@ -0,0 +1,14 @@
+# Introdução a formatação de strings
+
+nome = 'Bruno Vidal'
+altura = 1.72
+peso = 82
+imc = peso / altura ** 2
+
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu IMC é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
