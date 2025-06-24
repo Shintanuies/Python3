0ee940239862bf2ed74798551e8960ac211424e6
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-24 11:10:28 -0300

    include aula.31.py

---
+++ logica_de_programacao_basica/aula31.py
@@ -0,0 +1,25 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Nçao valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade
+Obs: Não declartar variáveis dentro de condicionais
+"""
+
+# v1 = 'a'
+# v2 = 'a'
+# print(id(v1))
+# print(id(v2))
+
+
+condicao = False
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+print(passou_no_if, passou_no_if is None)
+print(passou_no_if, passou_no_if is not None)
